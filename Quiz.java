3fee7f0aadacefc9ef6fe6f1622948c9d0d27344
Marie Krüger <73947286+kruegermarie@users.noreply.github.com>
2021-01-27 16:20:18 +0100

    add question to quiz.java

--- Quiz.java
+++ Quiz.java
@@ -1,7 +1,28 @@
 
         public class Quiz {
 
-			public static void main(String[] args) {}
+			public static void main(String[] args) {
 
-		}
+			
+
+			
+			String[] question2 = {
+				"Erste Frage?",
+				"<1> Antwort A",
+				"<2> Antwort B",
+				"<3> Antwort C",
+				"C"
+		};
+
+		String[] question1 = {
+				"Zweite Frage?",
+				"<1> A",
+				"<2> B",
+				"<3> C",
+				"A"
+		};
+
+	}
+	
+}
 
