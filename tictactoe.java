7f26b84f1ea7536167da723039e4690a5ac62ab5
Marie Krüger <73947286+kruegermarie@users.noreply.github.com>
2021-02-06 19:32:25 +0100

    add first game round

--- tictactoe.java
+++ tictactoe.java
@@ -15,17 +15,14 @@ public class progmeth {
 			
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Wo moechtest du setzen? (1-9)");
-		int pos = scan.nextInt();
+		int playerPos = scan.nextInt(); //playerpos //cpupos
 			
-		System.out.println(pos);
 
-		place (Spielbrett, pos, "player");
-		
+		place (Spielbrett, playerPos, "player");
 		
 		Random rand = new Random();
-
+		int cpuPos = rand.nextInt(9) + 1;
 		place (Spielbrett, pos, "cpu"); //pos eingabe player = random
-		
 		
 		printSpielbrett(Spielbrett);
 
@@ -45,7 +42,6 @@ public class progmeth {
 }
 		public static void place(char[][] Spielbrett, int pos, String user) {
 
-
 			//owner of X and O
 			char symbol = ' ';
 			
@@ -58,31 +54,33 @@ public class progmeth {
 
 			switch(pos) {
 				case 1: 
-					Spielbrett[0][0] = 'X';
+					Spielbrett[0][0] = symbol;
 					break;
 				case 2: 
-					Spielbrett[0][2] = 'X';
+					Spielbrett[0][2] = symbol;
 					break;
 				case 3: 
-					Spielbrett[0][4] = 'X';
+					Spielbrett[0][4] = symbol;
 					break;
 				case 4: 
-					Spielbrett[2][0] = 'X';
+					Spielbrett[2][0] = symbol;
 					break;
 				case 5: 
-					Spielbrett[2][2] = 'X';
+					Spielbrett[2][2] = symbol;
 					break;
 				case 6: 
-					Spielbrett[2][4] = 'X';
+					Spielbrett[2][4] = symbol;
 					break;
 				case 7: 
-					Spielbrett[4][0] = 'X';
+					Spielbrett[4][0] = symbol;
 					break;
 				case 8: 
-					Spielbrett[4][2] = 'X';
+					Spielbrett[4][2] = symbol;
 					break;
 				case 9: 
-					Spielbrett[4][4] = 'X';
+					Spielbrett[4][4] = symbol;
+					break;
+				default: 
 					break;
 			    }
 
