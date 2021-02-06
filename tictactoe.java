29e3374c3855e9d4cd0c18c80dae40db9f14b932
Marie Krüger <73947286+kruegermarie@users.noreply.github.com>
2021-02-06 16:45:26 +0100

    add methode for Spielbrett

--- tictactoe.java
+++ tictactoe.java
@@ -1,4 +1,3 @@
-
 public class progmeth {
 
 	public static void main(String[] args) {
@@ -10,13 +9,18 @@ public class progmeth {
 	               {'-', '+', '-', '+', '-'}, 
 	               {' ', '|', ' ', '|', ' '}};
 		
+	printSpielbrett(Spielbrett);
+	
+	}
+		
+	public static void printSpielbrett(char [][] Spielbrett ) {
 		for(char [] row: Spielbrett) {
 			for(char c : row) {
 				System.out.print(c);
 
 			}
 			System.out.println();
-			
+				
 			
 			
 			
