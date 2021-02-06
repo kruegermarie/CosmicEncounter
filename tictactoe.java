629b11cb41f5a49d1f0a4fefa129fe22c403e78b
Marie Krüger <73947286+kruegermarie@users.noreply.github.com>
2021-02-06 17:41:16 +0100

    add switch case

--- tictactoe.java
+++ tictactoe.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class progmeth {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		char[][] Spielbrett = {{' ', '|', ' ', '|', ' '}, 
 	               {'-', '+', '-', '+', '-'},
@@ -12,14 +11,49 @@ public class progmeth {
 	               {' ', '|', ' ', '|', ' '}};
 		
 	printSpielbrett(Spielbrett);
-			Scanner scan = new Scanner(System.in);
-			System.out.println("Wo moechtest du setzen? (1-9)");
-			int pos = scan.nextInt();
 			
-			System.out.println(pos);
-			//Ausgabe von dem was eingegeben wurde
+	
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Wo moechtest du setzen? (1-9)");
+		int pos = scan.nextInt();
 			
-	}
+		System.out.println(pos);
+			
+		switch(pos) {
+			case 1: 
+				Spielbrett[0][0] = 'X';
+				break;
+			case 2: 
+				Spielbrett[0][2] = 'X';
+				break;
+			case 3: 
+				Spielbrett[0][4] = 'X';
+				break;
+			case 4: 
+				Spielbrett[2][0] = 'X';
+				break;
+			case 5: 
+				Spielbrett[2][2] = 'X';
+				break;
+			case 6: 
+				Spielbrett[2][4] = 'X';
+				break;
+			case 7: 
+				Spielbrett[4][0] = 'X';
+				break;
+			case 8: 
+				Spielbrett[4][2] = 'X';
+				break;
+			case 9: 
+				Spielbrett[4][4] = 'X';
+				break;
+				}
+			
+			
+				printSpielbrett(Spielbrett);
+			
+		}
+	
 		
 	public static void printSpielbrett(char [][] Spielbrett ) {
 		for(char [] row: Spielbrett) {
